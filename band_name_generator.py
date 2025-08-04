65a6427ea7863216f0497fe83e322a2bff2b4c77
genomicinvader <anikangel664@gmail.com>
2025-08-04 15:04:52 +0600

    band_name variable is added

--- band_name_generator.py
+++ band_name_generator.py
@@ -1,3 +1,4 @@
 print("Welcome to our Band Name Generator App!\n")
 city = input("What's the name of the city you grew up in?\n")
 pet = input("What's the name of your pet?\n")
+band_name = f"{city} {pet}"
