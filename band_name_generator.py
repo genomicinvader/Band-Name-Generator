d2e01874aa43f4317e66fc83636481bfa120d0e0
genomicinvader <anikangel664@gmail.com>
2025-08-04 15:05:50 +0600

    Add output for generated band name and closing message

--- band_name_generator.py
+++ band_name_generator.py
@@ -2,3 +2,5 @@ print("Welcome to our Band Name Generator App!\n")
 city = input("What's the name of the city you grew up in?\n")
 pet = input("What's the name of your pet?\n")
 band_name = f"{city} {pet}"
+print(f"Your band name could be: {band_name}\nThanks for using our app!\n")
+print("Have a great day!")
